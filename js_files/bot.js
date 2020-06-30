1d1b5c82c36ec97928c35bf227efcdb95f6cbb9b
RishitOMG <yogamer0101@gmail.com>
2020-06-30 09:18:43 +0530

    making my edits

---
+++ js_files/bot.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log("A Bot, By Panda at your service!");
+
+  client.user.setActivity("by Helping :D")
+  client.guilds.cache.forEach((guild) => {
+    console.log(guild.name);
+  bot_commands = '727206999317676132'
+  genral = '727206852923883551'
+});
+  let genralChannel = client.channels.cache.get(genral)
+  let botChannel = client.channels.cache.get(bot_commands)
+// https://drive.google.com/file/d/1Aj0KrUrMt40Q4WgQ63Gzrmcl_PZai3go/view?usp=sharing
+  botChannel.send("I am online boiiiii!")
+})
+
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new Discord.MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
+
+client.login("NzI3MjA4MTI4MDcxOTkxMzA3.Xvogtg.uqhxBQXUIhxG3e6sISexzEQO8C4")
