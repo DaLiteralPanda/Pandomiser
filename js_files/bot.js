636d090f54608f9aaf27ac47712abf6a14004c0b
RishitOMG <yogamer0101@gmail.com>
2020-07-01 21:52:58 +0530

    Commiting my changes

--- js_files/bot.js
+++ js_files/bot.js
@@ -1,5 +1,6 @@
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, Discord, ClientUser } = require('discord.js');
 const client = new Client();
+//prefix = ';'
 
 client.on('ready', () => {
   console.log("Bot Online!");
@@ -12,25 +13,23 @@ client.on('ready', () => {
 });
   let genralChannel = client.channels.cache.get(genral);
   let botChannel = client.channels.cache.get(bot_commands);
-// https://drive.google.com/file/d/1Aj0KrUrMt40Q4WgQ63Gzrmcl_PZai3go/view?usp=sharing
-  botChannel.send("I am online boiiiii!");
+  //botChannel.send("I am online boiii!");
 });
 
 client.on('message', message => {
-  // If the message is "how to embed"
-  if (message.content === 'how to embed') {
+  if (message.content === `;help`) {
     // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
+    const help = new MessageEmbed()
       // Set the title of the field
-      .setTitle('A slick little embed')
+      .setTitle('Help Command')
       // Set the color of the embed
-      .setColor(0xff0000)
+      .setColor('#bbdf32')
       // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
+      .addFields(
+        { name: 'Want to generate random words?', value: `;randomw`},
+        { name: 'Want to generate some random questions?', value: `;randomq`},
+      )
+    message.channel.send(help);
   }
 });
 
