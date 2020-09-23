485611a00a74945f5f3b235703215c06f9aa5f81
DaLiteralPanda <yogamer0101@gmail.com>
2020-09-23 15:53:30 +0530

    Made a command loader...

---
+++ commands/random_range.js
@@ -0,0 +1,31 @@
+module.exports = {
+	name: 'randomRange',
+	description: 'Generates Random Numbers',
+	execute(message, args) {
+    const gs = message.content.replace(`${prefix}randomRange`, "").split(" ").slice(1);
+
+    if(gs[0]) {
+      let min, max;
+
+	  // There is only one argument
+      if(!gs[1]) {
+        min = 0;
+        max = Number(gs[0]);
+      }
+	  // Two arguments
+      else {
+        min = Number(gs[0]);
+        max = Number(gs[1]);
+
+        // Minimum is actually maximum, so swap
+        if(min > max) {
+          max = min;
+          min = 0;
+	    }
+      }
+
+      const num = Math.floor(Math.random() * (max - min)) + min;
+      message.channel.send(num);
+    }
+	},
+};
--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-const { Client, MessageEmbed, Discord, ClientUser } = require('discord.js');
-const client = new Client();
 const fs = require('fs');
 const path = require('path');
-const prefix = "p!"
-const quotePath = __dirname + '/data/quotes.txt';
-const questionPath = __dirname + '/data/questions.txt';
-const wordPath = __dirname + '/data/words.txt';
+const { Client, MessageEmbed, Discord, ClientUser, Collection } = require('discord.js');
+const client = new Client();
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const prefix = "p!";
 const Trello = require("trello")
 const trello = new Trello(process.env.Trello_KEY, process.env.Trello_User_KEY);
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
 
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+// Login
 client.login(process.env.KEY);
 
 const activity_list = [
@@ -17,7 +24,9 @@ const activity_list = [
   `${prefix}invite`,
   `${client.guilds.cache.size} servers!`
 ]
-// process.env.KEY
+
+
+
 client.on('ready', () => {
   const activity_list = [
     `${prefix}help`,
@@ -36,6 +45,7 @@ client.on('ready', () => {
   });
 })
 
+// Invite embed
 const embedInvite = new MessageEmbed()
   .setURL("https://discord.com/api/oauth2/authorize?client_id=727208128071991307&permissions=387136&scope=bot")
   .setColor("bbdf32")
@@ -44,6 +54,7 @@ const embedInvite = new MessageEmbed()
   .setFooter("Made by DaLiteralPanda#9453 | riad#9084 | William_#3483")
   .addField("Thanks for inviting me to your server!", "[Invite link](https://discord.com/api/oauth2/authorize?client_id=727208128071991307&permissions=387136&scope=bot)");
 
+// Help embed
 const help = new MessageEmbed()
   .setTitle("Help!")
   .setThumbnail("https://media.discordapp.net/attachments/731529488671703142/731538158403059792/improved_logo.jpg")
@@ -51,79 +62,38 @@ const help = new MessageEmbed()
   .addField("Want a random word?", `${prefix}randomw`)
   .addField("How about a random question?", `${prefix}randomq`)
   .addField("A random *fake* quote?", `${prefix}randomQuote`)
+  .addField("Want a random number from a range of numbers?", `${prefix}randomRange`)
   .addField("Something you'd like to see added to me?", `${prefix}suggestion [suggestion]`)
   .addField("Want me in your server?", `${prefix}invite`)
   .addField("Want to be in my server?", `${prefix}support`)
 
 client.on('message', message => {
+
+  // Command Loader
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
+  // Invite command
   if (message.content === `${prefix}invite`) {
     message.channel.send(embedInvite);
   };
-
+  // Help command
   if (message.content === `${prefix}help`) {
     message.channel.send(help);
   };
 
-  if (message.content === `${prefix}randomw`) {
-    fs.readFile(wordPath, 'utf8', function(err, data) {
-      let words = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
-      message.channel.send(words)
-    })
-  };
-
-  if (message.content === `${prefix}randomq`) {
-    fs.readFile(questionPath, 'utf8', function(err, data) { 
-      let questions = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
-      message.channel.send(questions);
-    })
-  };
-
-  if (message.content === `${prefix}randomQuote`) {
-    fs.readFile(quotePath, 'utf8', function(err, data) {
-      let markovQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
-      message.channel.send(markovQuotes);
-    })
-  };
-  
-  if (message.content === `${prefix}support`) {
-    const embed = new MessageEmbed()
-      .setURL("https://discord.gg/F4RUHc3")
-      .setColor("bbdf32")
-      .setAuthor("Pandomiser", "https://media.discordapp.net/attachments/731529488671703142/731538158403059792/improved_logo.jpg")
-      .setThumbnail("https://media.discordapp.net/attachments/731529488671703142/731538158403059792/improved_logo.jpg")
-      .setFooter("Made by <@478903410159255572> | <@579013278047535115> | <@579292491606523914>")
-      .addField("Join our support server if you think I am acting incorrectly!", "[Invite link](https://discord.gg/F4RUHc3)");
-    message.channel.send(embed)
-  }
-
-  if(message.content.startsWith(`${prefix}randomRange`)) {
-    const args = message.content.replace(`${prefix}randomRange`, "").split(" ").slice(1);
-
-    if(args[0]) {
-      let min, max;
-
-	  // There is only one argument
-      if(!args[1]) {
-        min = 0;
-        max = Number(args[0]);
-      }
-	  // Two arguments
-      else {
-        min = Number(args[0]);
-        max = Number(args[1]);
-
-        // Minimum is actually maximum, so swap
-        if(min > max) {
-          max = min;
-          min = 0;
-	    }
-      }
-
-      const num = Math.floor(Math.random() * (max - min)) + min;
-      message.channel.send(num);
-    }
-  }
-
+// Suggestion command
   if (message.content.startsWith(`${prefix}suggestion`)) {
     const suggestion = message.content.replace(`${prefix}suggestion `, "");
     if (suggestion === "") {
@@ -145,5 +115,5 @@ client.on('messageReactionAdd', async message => {
     .then(async collected => {
       if (collected.first().author.id !== "579292491606523914" || collected.first().author.id !== "579013278047535115" || collected.first().author.id !== "478903410159255572" && message.channel.id !== "730476984194433163") return false;
       trello.addCard(message.content, function(err) {if (err) console.log(err)})
-    })
-})
+    });
+});
