55b2164da982f31971823e9312f050823340b226
DaLiteralPanda <yogamer0101@gmail.com>
2020-09-23 16:29:59 +0530

    commiting my changes

--- commands/randomQuote.js
+++ commands/randomQuote.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 module.exports = {
 	name: 'randomQuote',
-	description: 'Generates Random *fake* Quotes',
+	description: 'Generates Random fake Quotes',
 	execute(message, args) {
 		const quotePath = path.join( __dirname, '../data/quotes.txt');
     fs.readFile(quotePath, 'utf8', function(err, data) {
