408794f5a4261b3bbb61cb41d22ac8f63e0840f5
iddev5 <ayushtechportal@gmail.com>
2020-09-23 17:45:00 +0530

    Fix issue with command with Uppercase letters not working

--- commands/randomQuote.js
+++ commands/randomQuote.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'randomQuote',
 	description: 'Generates Random fake Quotes',
 	execute(message, args) {
-		const quotePath = path.join( __dirname, '../data/quotes.txt');
+	const quotePath = path.join( __dirname, '../data/quotes.txt');
     fs.readFile(quotePath, 'utf8', function(err, data) {
       let markovQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
       message.channel.send(markovQuotes);
