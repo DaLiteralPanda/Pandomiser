81fea3eaa928371cdbd9102b56d20037ab6722d8
DaLiteralPanda <yogamer0101@gmail.com>
2020-09-24 09:43:23 +0530

    commiting my changes

--- commands/random_question.js
+++ commands/random_question.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'randomq',
 	description: 'Generates Random Questions',
 	execute(message, args) {
-		const questionPath = path.join( __dirname, '../data/questions.txt');
+	const questionPath = path.join( __dirname, '../data/questions.txt');
     fs.readFile(questionPath, 'utf8', function(err, data) {
       let questions = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
       message.channel.send(questions);
