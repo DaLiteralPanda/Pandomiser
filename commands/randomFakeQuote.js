b01c6917d3e6c25411134111d13eeaaa1f225261
DaLiteralPanda <yogamer0101@gmail.com>
2021-01-10 17:59:24 +0530

    Made randomFakeCommand and randomQuote(real) command actually its kinda copied or smthing idk lmao

---
+++ commands/randomFakeQuote.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+module.exports = {
+	name: 'randomFakeQuote',
+	description: 'Generates a Random fake Quotes',
+	execute(message, args) {
+	const quotePath = path.join( __dirname, '../data/fakeQuotes.txt');
+    fs.readFile(quotePath, 'utf8', function(err, data) {
+      let markovQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
+      message.channel.send(markovQuotes);
+    });
+	},
+};
---
+++ commands/randomQuote.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+module.exports = {
+	name: 'randomQuote',
+	description: 'Generates a Random Quotes',
+	execute(message, args) {
+	const quotePath = path.join( __dirname, '../data/realQuotes.txt');
+    fs.readFile(quotePath, 'utf8', function(err, data) {
+      let realQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
+      message.channel.send(realQuotes);
+    });
+	},
+};
