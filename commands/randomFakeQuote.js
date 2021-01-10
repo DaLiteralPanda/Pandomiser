c9a8a744555ff27a5117b399aafd6e378ebae3c3
DaLiteralPanda <yogamer0101@gmail.com>
2021-01-10 18:22:12 +0530

    Commiting my changes

--- commands/randomFakeQuote.js
+++ commands/randomFakeQuote.js
@@ -4,10 +4,10 @@ module.exports = {
 	name: 'randomFakeQuote',
 	description: 'Generates a Random fake Quotes',
 	execute(message, args) {
-	const quotePath = path.join( __dirname, '../data/fakeQuotes.txt');
-    fs.readFile(quotePath, 'utf8', function(err, data) {
-      let markovQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
-      message.channel.send(markovQuotes);
-    });
+		const quotePath = path.join( __dirname, '../data/fakeQuotes.txt');
+		fs.readFile(quotePath, 'utf8', function(err, data) {
+			let markovQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
+			message.channel.send(markovQuotes);
+		});
 	},
 };
--- commands/randomQuote.js
+++ commands/randomQuote.js
@@ -4,10 +4,10 @@ module.exports = {
 	name: 'randomQuote',
 	description: 'Generates a Random Quotes',
 	execute(message, args) {
-	const quotePath = path.join( __dirname, '../data/realQuotes.txt');
-    fs.readFile(quotePath, 'utf8', function(err, data) {
-      let realQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
-      message.channel.send(realQuotes);
-    });
+		const quotePath = path.join( __dirname, '../data/realQuotes.txt');
+		fs.readFile(quotePath, 'utf8', function(err, data) {
+			let realQuotes = data.split("\n")[Math.floor(Math.random() * data.split("\n").length)]
+			message.channel.send(realQuotes);
+		});
 	},
 };
